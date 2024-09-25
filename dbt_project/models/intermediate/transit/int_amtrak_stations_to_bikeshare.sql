27430406bdcfce9e6f3176e7fa230dc7bf868d1e
Christopher Manning <289331+christophermanning@users.noreply.github.com>
2024-09-24 21:30:53 -0500

    add amtrak_stations_near_bikeshare notebook and queries

---
+++ dbt_project/models/intermediate/transit/int_amtrak_stations_to_bikeshare.sql
@@ -0,0 +1,39 @@
+with
+
+amtrak_stations as (
+
+  select * from {{ ref('stg_dot__amtrak_stations') }}
+
+)
+
+, bikeshare as (
+
+  select * from {{ ref('stg_dot__bikeshare') }}
+
+)
+
+, final as (
+
+  select
+    s.code
+    , s.name as station_name
+    , st_x(s.geom) as station_lat
+    , st_y(s.geom) as station_lon
+    , b.name as bikeshare_name
+    , st_x(b.geom) as bikeshare_lat
+    , st_y(b.geom) as bikeshare_lon
+    , st_distance_sphere(b.geom, s.geom)::int as dist
+
+  from amtrak_stations s
+  left join bikeshare b on 1 = 1
+
+  where
+    -- bikeshare farther than a mile from an amtrack station isn't relevant
+    dist <= 1600
+
+    and station_type = 'TRAIN'
+
+  order by dist
+)
+
+select * from final
--- dbt_project/models/marts/transit/amtrak_stations_near_bikeshare.sql
+++ dbt_project/models/marts/transit/amtrak_stations_near_bikeshare.sql
@@ -12,35 +12,22 @@ amtrak_stations as (
 
 )
 
-, stations as (
-
-  select
-    s.code
-    , s.name as station_name
-    , b.name
-    , ST_DISTANCE_SPHERE(b.geom, s.geom)::int as dist
-
-  from amtrak_stations s
-  left join bikeshare b on 1 = 1
-
-  where
-    dist < 400
-
-    and station_type = 'TRAIN'
-
-  order by dist
-)
-
 , final as (
 
   select
     code
-    , station_name
-    , name
+    , station_name as name
+    , station_lat as lat
+    , station_lon as lon
+    , bikeshare_name
     , COUNT(*) as num_stations
     , MIN(dist) as min_distance
-  from stations
-  group by 1, 2, 3
+  from {{ ref('int_amtrak_stations_to_bikeshare') }}
+
+  -- one lap of a standard running track is the threshold for considering it to be nearby
+  where dist <= 400
+
+  group by 1, 2, 3, 4, 5
   order by code
 
 )
